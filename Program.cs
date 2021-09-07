fe3c99f4a8db21643db4d1109f9753d5193e7642
Md. Sumon Mia <R041604014@recursion.com.bd>
2021-09-07 19:46:45 +0600

    oop university work done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArrongShop
 {
@@ -6,18 +7,43 @@ namespace ArrongShop
     {
         static void Main(string[] args)
         {
-            SalaryEmployee salaryEmployee = new SalaryEmployee("md", "sumon", "123", 5000);
+          //  SalaryEmployee salaryEmployee = new SalaryEmployee("md", "sumon", "123", 5000);
 
 
-            Console.WriteLine(salaryEmployee.getFullInformation());
+          //  Console.WriteLine(salaryEmployee.getFullInformation());
 
 
-            CommissionEmployee commissionEmployee = new CommissionEmployee("md", "sumon", "123", 500000, 5);
+            // CommissionEmployee commissionEmployee = new CommissionEmployee("md", "sumon", "123", 500000, 5);
+            //
+            // Console.WriteLine(commissionEmployee.getFullInformation());
 
-            Console.WriteLine(commissionEmployee.getFullInformation());
+
+            Employee salaryEmployee = new SalaryEmployee("md", "sumon", "123", 5000);
+         //   Console.WriteLine(salaryEmployee.getFullInformation());
 
 
-            
+
+            Employee commissionEmployee = new CommissionEmployee("md", "sumon", "123", 500000, 5);
+
+       //     Console.WriteLine(commissionEmployee.getFullInformation());
+
+
+            var employee = new List<Employee>();
+
+            employee.Add(salaryEmployee);
+            employee.Add(commissionEmployee);
+
+            foreach (var employee1 in employee)
+            {
+                Console.WriteLine(employee1.getEarning());
+            }
+
+
+            int number1 = 50;
+            decimal number2 = number1;
+
+            int number3 = (int) number2;
+
 
             Console.ReadKey();
 
